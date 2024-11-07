108ab5e974b9a1dcab616eb9fff47b7649fb5d37
Blond <marcio-augusto22@hotmail.com>
2024-11-07 12:15:54 -0300

    Commit inicial

---
+++ java.js
@@ -0,0 +1,5 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+    const pieChart = document.getElementById('pie-chart');
+    pieChart.style.background = 'conic-gradient(red 75%, green 75%)';
+});
